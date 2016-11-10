25591cc619802893c776f81888fd118c06053a0c
jamesknelson <james@jamesknelson.com>
2016-11-10 20:53:32 +0900

    Handle URL encoded params

--- src/utils/SerializationUtils.js
+++ src/utils/SerializationUtils.js
@@ -5,7 +5,7 @@ export function deserializeParams(paramTypes, params) {
     const key = keys[i]
     const serializer = paramTypes[key].serializer
     const serializedValue = params[key]
-    deserializedParams[key] = serializer.deserialize(serializedValue)
+    deserializedParams[key] = decodeURIComponent(serializer.deserialize(serializedValue))
   }
   return deserializedParams
 }
@@ -15,7 +15,7 @@ export function serializeParams(paramTypes, params) {
   const keys = Object.keys(params)
   for (let i = 0, len = keys.length; i < len; i++) {
     const key = keys[i]
-    const serializedValue = paramTypes[key].serializer.serialize(params[key])
+    const serializedValue = encodeURIComponent(paramTypes[key].serializer.serialize(params[key]))
     if (serializedValue !== undefined) {
       serializedParams[key] = serializedValue
     }
