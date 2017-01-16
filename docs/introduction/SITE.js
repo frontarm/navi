406df4d3183ee24f03d56d3f5eb41cb354f1b068
jamesknelson <james@jamesknelson.com>
2017-01-16 22:16:30 +0900

    flesh out 'What You Get' in introduction

--- docs/introduction/SITE.js
+++ docs/introduction/SITE.js
@@ -2,8 +2,8 @@ module.exports = {
   title: 'Introduction',
   index: [
     require('./do-i-need-a-router.md'),
-    require('./Motivation.md'),
+    require('./motivation.md'),
     require('./three-principles.md'),
-    require('./locations-routes-and-maps.md'),
+    require('./what-you-get.md'),
   ],
 }
