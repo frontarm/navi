bd469a7378a31cbe9a04db59d908dfca0314a49a
jamesknelson <james@jamesknelson.com>
2017-01-17 22:09:49 +0900

    what is a Junction

--- docs/introduction/SITE.js
+++ docs/introduction/SITE.js
@@ -4,6 +4,6 @@ module.exports = {
     require('./do-i-need-a-router.md'),
     require('./motivation.md'),
     require('./three-principles.md'),
-    require('./what-you-get.md'),
+    require('./what-is-a-junction.md'),
   ],
 }
