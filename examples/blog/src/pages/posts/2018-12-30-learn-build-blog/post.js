862ca78556b598c013cc33a2e89e97d7b01444c5
James K Nelson <james@jamesknelson.com>
2019-01-05 00:10:49 +0800

    blog example: update welcome posts

--- examples/blog/src/pages/posts/2018-12-30-learn-build-blog/post.js
+++
@@ -1,6 +0,0 @@
-export default {
-  title: `Learn How To Build This Blog From Scratch`,
-  tags: ['react', 'navi'],
-  spoiler: "An interactive course with loads of live examples and exercises.",
-  getContent: () => import('./document.mdx'),
-}
