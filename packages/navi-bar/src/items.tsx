4193add99e27e2f447f3ae6f93d4f19b68c6b79e
James K Nelson <james@jamesknelson.com>
2019-02-09 19:25:16 +0900

    [navi-bar] update to work with 0.11

--- packages/navi-bar/src/items.tsx
+++ packages/navi-bar/src/items.tsx
@@ -1,52 +1,49 @@
 import * as Navi from 'navi'
 
-export interface SwitchItem {
+export interface GroupItem {
   level: number
-  meta: any
+  data: any
   children: Item[]
   url: Navi.URLDescriptor
   title?: string
-  type: ItemType.Switch
+  type: 'group'
 }
 
 export interface PageItem {
   level: number
-  meta: any
+  data: any
   url: Navi.URLDescriptor
   title: string
-  type: ItemType.Page
+  type: 'page'
 }
 
-export type Item = PageItem | SwitchItem
+export type Item = PageItem | GroupItem
 
-export enum ItemType {
-  Switch = 'switch',
-  Page = 'page',
-}
+export type ItemType = 'group' | 'page'
 
 export function getItems(
-  pageMap: Navi.PageMap,
+  routeMap: Navi.RouteMap,
   comparator?: (x: Item, y: Item) => -1 | 0 | 1,
 ): Item[] {
-  let pages = Object.entries(pageMap)
+  let routes = Object.values(routeMap)
 
   // Find the deepest switch that every page has in common
   let switchCounts = new Map<string, number>()
   let switchPositions = [] as string[]
   let deepestSwitchIndex = 0
-  for (let i = 0; i < pages.length; i++) {
-    let pageRoute = pages[i][1]
-    let segments = pageRoute.segments.slice(0, -1) as Navi.SwitchSegment[]
+  for (let i = 0; i < routes.length; i++) {
+    let pageRoute = routes[i]
+    let segments = pageRoute.segments.slice(0, -1) as Navi.Segment[]
     for (let j = 0; j < segments.length; j++) {
       let naviSwitch = segments[j].url.pathname
       let nextCount = (switchCounts.get(naviSwitch) || 0) + 1
       if (j > deepestSwitchIndex) {
         deepestSwitchIndex = j
       }
-      if (nextCount === pages.length) {
+      if (nextCount === routes.length) {
         // This switch has appeared in all pages, so we can add it
         switchPositions[j] = naviSwitch
-      } else if (nextCount < pages.length) {
+      } else if (nextCount < routes.length) {
         switchCounts.set(naviSwitch, nextCount)
       }
     }
@@ -56,38 +53,41 @@ export function getItems(
   let rootSwitch = switchPositions[rootSwitchIndex]
   let rootItems: Item[] = []
   let switchItems = new Map<string, Item[]>([[rootSwitch, rootItems]])
-
+  let partialRoute: Navi.Route = routes[0].segments.slice(0, rootSwitchIndex + 1).reduce(Navi.defaultRouteReducer, undefined)!
   // Create a tree by iterating over each segment below the root segment
   // for each route.
-  for (let i = 0; i < pages.length; i++) {
-    let [pathname, pageRoute] = pages[i]
+  for (let i = 0; i < routes.length; i++) {
+    let pageRoute = routes[i]
     let segments = pageRoute.segments.slice(rootSwitchIndex + 1)
     let lastSwitch = rootSwitch
+    let route = { ...partialRoute }
     for (let j = 0; j < segments.length; j++) {
       let segment = segments[j]
-      if (segment.type === Navi.SegmentType.Switch) {
+      route = Navi.defaultRouteReducer(route, segment)
+      if (segment.type === 'map' && (segment.patterns.length > 1 || segment.patterns[0] !== '/')) {
         let segmentSwitch = segment.url.pathname
         if (!switchItems.has(segmentSwitch)) {
-          let item: SwitchItem = {
+          let item: GroupItem = {
             level: j + 1,
-            meta: segment.meta,
+            data: route.data,
             children: [],
             url: segment.url,
-            title: segment.title,
-            type: ItemType.Switch,
+            title: route.title,
+            type: 'group',
           }
           switchItems.get(lastSwitch)!.push(item)
           switchItems.set(segmentSwitch, item.children)
         }
         lastSwitch = segmentSwitch
-      } else {
+      }
+      else if (j === segments.length - 1) {
         let items = switchItems.get(lastSwitch)!
         items.push({
           level: j + 1,
-          meta: segment.meta,
+          data: route.data,
           url: segment.url,
-          title: segment.title!,
-          type: ItemType.Page,
+          title: route.title!,
+          type: 'page',
         })
       }
     }
