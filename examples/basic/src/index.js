e388b4f67ed3bfd2a0da2da946e6585c15c23cb2
James K Nelson <james@jamesknelson.com>
2019-07-10 19:50:04 +0900

    add react-navi-helmet to basic example

--- examples/basic/src/index.js
+++ examples/basic/src/index.js
@@ -1,23 +1,27 @@
-import React, { Suspense } from "react"
-import ReactDOM from "react-dom"
-import { Router, View } from "react-navi"
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom'
+import { Router, View } from 'react-navi'
+import reactNaviHelmet from 'react-navi-helmet'
 import Layout from './components/AppLayout'
-import "./index.css"
-import routes from "./routes"
-import * as serviceWorker from "./serviceWorker"
+import './index.css'
+import routes from './routes'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <Router hashScrollBehavior='smooth' routes={routes}>
+  <Router
+    hashScrollBehavior="smooth"
+    routes={routes}
+    renderViewHead={reactNaviHelmet}>
     <Layout>
       <Suspense fallback={null}>
         <View />
       </Suspense>
     </Layout>
   </Router>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
