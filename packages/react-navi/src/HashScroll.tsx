2da0f6d6ee28c88ea7bd383bbc2820028ba7484f
James K Nelson <james@jamesknelson.com>
2019-12-26 17:50:59 +0900

    allow for custom scroll handlers, and for scroll to be disabled. fixes #154

--- packages/react-navi/src/HashScroll.tsx
+++ packages/react-navi/src/HashScroll.tsx
@@ -1,55 +1,76 @@
 import * as React from 'react'
 
-export type HashScrollBehavior = 'smooth' | 'auto'
+export type HashScrollBehavior =
+  | 'smooth'
+  | 'auto'
+  | 'none'
+  | ((hash: string | undefined | null) => void)
 
 export const HashScrollContext = React.createContext<HashScrollBehavior>('auto')
 
-export interface HashScrollProps { behavior?: HashScrollBehavior, children: React.ReactNode }
+export interface HashScrollProps {
+  behavior?: HashScrollBehavior
+  children: React.ReactNode
+}
 
 export function HashScroll(props: HashScrollProps) {
-    if (!props.behavior) {
-        return <>{props.children}</>
-    }
+  if (!props.behavior) {
+    return <>{props.children}</>
+  }
 
-    return (
-        <HashScrollContext.Provider value={props.behavior}>
-            {props.children}
-        </HashScrollContext.Provider>
-    )
+  return (
+    <HashScrollContext.Provider value={props.behavior}>
+      {props.children}
+    </HashScrollContext.Provider>
+  )
 }
 
-function scroll(left: number, top: number, behavior: HashScrollBehavior) {
-    if (behavior === 'auto') {
-        window.scroll(left, top)
-    }
-    else {
-        try {
-            window.scroll({
-                top,
-                left,
-                behavior,
-            })
-        }
-        catch (e) {
-            window.scroll(left, top)
-        }
-    }
+function smoothScroll(left: number, top: number) {
+  try {
+    window.scroll({
+      top,
+      left,
+      behavior: 'smooth',
+    })
+  } catch (e) {
+    window.scroll(left, top)
+  }
 }
 
-export function scrollToHash(hash: string | undefined | null, behavior: HashScrollBehavior = 'auto') {
+const behaviors = {
+  none: () => {},
+  auto: (hash: string | undefined | null) => {
     if (hash) {
-        let id = document.getElementById(hash.slice(1))
-        if (id) {
-            let { top, left } = id.getBoundingClientRect()
-
-            scroll(left + window.pageXOffset, top + window.pageYOffset, behavior)
-
-            // Focus the element, as default behavior is cancelled.
-            // https://css-tricks.com/snippets/jquery/smooth-scrolling/
-            id.focus()
-        }
+      let id = document.getElementById(hash.slice(1))
+      if (id) {
+        let { top, left } = id.getBoundingClientRect()
+        window.scroll(left + window.pageXOffset, top + window.pageYOffset)
+      }
+    } else {
+      window.scroll(0, 0)
     }
-    else {
-        scroll(0, 0, behavior)
+  },
+  smooth: (hash: string | undefined | null) => {
+    if (hash) {
+      let id = document.getElementById(hash.slice(1))
+      if (id) {
+        let { top, left } = id.getBoundingClientRect()
+        smoothScroll(left + window.pageXOffset, top + window.pageYOffset)
+
+        // Focus the element, as default behavior is cancelled.
+        // https://css-tricks.com/snippets/jquery/smooth-scrolling/
+        id.focus()
+      }
+    } else {
+      smoothScroll(0, 0)
     }
+  },
+}
+
+export function scrollToHash(
+  hash: string | undefined | null,
+  behavior: HashScrollBehavior = 'auto',
+) {
+  const fn = typeof behavior === 'string' ? behaviors[behavior] : behavior
+  fn(hash)
 }
